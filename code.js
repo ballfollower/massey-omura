daccfd6b6b03f0e745cf25ecec724b98b16d5271
Gregdeg <gregdeg@op.pl>
2018-04-27 16:10:34 +0200

    Generating encryption keys

--- code.js
+++ code.js
@@ -1,21 +1,31 @@
 $(function () {
     $('#drawingButton').on('click', function () {
         $("#p").val(randomProbablePrime($('#pBits').val()).toString(10));
+        $('#a').val(drawEncryptionKey(bigInt($("#p").val())));
+        $('#b').val(drawEncryptionKey(bigInt($("#p").val())));
     });
 });
 
 function randomProbablePrime(len) {
     var a;
-    var lowerBound=bigInt(2).pow(len-1);
-    var upperBound=bigInt(2).pow(len).prev();
+    const LOWER_BOUND=bigInt(2).pow(len-1);
+    const UPPER_BOUND=bigInt(2).pow(len).prev();
 
     do {
-        a=bigInt.randBetween(lowerBound, upperBound);
+        a=bigInt.randBetween(LOWER_BOUND, UPPER_BOUND);
     } while (!a.isProbablePrime());
 
     return a;
 }
 
-function drawEncryptionKey(){
-    
+function drawEncryptionKey(prime){
+    var key;
+    const LOWER_BOUND=bigInt(2);
+    const UPPER_BOUND=prime.subtract(2);
+
+    do {
+        key=bigInt.randBetween(LOWER_BOUND, UPPER_BOUND);
+    } while (bigInt.gcd(key,prime.prev()).compare(1));
+
+    return key;
 }
