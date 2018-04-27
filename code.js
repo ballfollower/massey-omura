7bbf06b134c380269a023970af98fcccc9f7a2dc
Gregdeg <gregdeg@op.pl>
2018-04-27 17:22:09 +0200

    Generating full setup

--- code.js
+++ code.js
@@ -1,8 +1,23 @@
 $(function () {
     $('#drawingButton').on('click', function () {
         $("#p").val(randomProbablePrime($('#pBits').val()).toString(10));
+        p=bigInt($("#p").val());
+        
         $('#a').val(drawEncryptionKey(bigInt($("#p").val())));
+        a=bigInt($('#a').val());
+        
         $('#b').val(drawEncryptionKey(bigInt($("#p").val())));
+        b=bigInt($('#b').val());
+        
+        $('#aInv').val(a.modInv(p.prev()));
+        aInv=bigInt($('#aInv').val());
+        
+        $('#bInv').val(b.modInv(p.prev()));
+        bInv=bigInt($('#bInv').val());
+    });
+    
+    $('#input').on('change', function(){
+        
     });
 });
 
