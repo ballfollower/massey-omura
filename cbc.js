f293c189df18f8e8eefaa2f2df61bef7c1c8bcff
Grzegorz Kłosiński <Grzegorz Kosiski>
2018-05-03 14:17:19 +0200

    Added cbc encryption

--- cbc.js
+++ cbc.js
@@ -1,16 +1,28 @@
 function cbc(plaintext){
     var iv=bigInt.randBetween(1,p.prev());
     
-    var ciphertextA = powerModArray(plaintext, a, p);
-    $('#aEncrypted').val(ciphertextA);
+//    var ciphertextA = powerModArray(plaintext, a, p);
+  //  $('#aEncrypted').val(ciphertextA);
 
-    var ciphertextAB = powerModArray(ciphertextA, b, p);
-    $('#bEncrypted').val(ciphertextAB);
+    //var ciphertextAB = powerModArray(ciphertextA, b, p);
+//    $('#bEncrypted').val(ciphertextAB);
 
-    var ciphertextABA = powerModArray(ciphertextAB, aInv, p);
-    $('#aDecrypted').val(ciphertextABA);
+//    var ciphertextABA = powerModArray(ciphertextAB, aInv, p);
+//    $('#aDecrypted').val(ciphertextABA);
 
-    var ciphertextABAB = powerModArray(ciphertextABA, bInv, p);
-    $('#outputEncoded').val(ciphertextABAB);
+//    var ciphertextABAB = powerModArray(ciphertextABA, bInv, p);
+//    $('#outputEncoded').val(ciphertextABAB);
 
     return ciphertextABAB;
+}
+
+function ecbEncrypt(numbers, exponent, modulus, iv) {
+    var result = [];
+
+    result.push(iv.xor(numbers[0].modPow(exponent, modulus)))
+    for (var i = 1; i < numbers.length; i++) {
+        result.push(numbers[i-1].xor(numbers[i].modPow(exponent, modulus)));
+    }
+
+    return result;
+}
