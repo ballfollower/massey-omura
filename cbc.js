7a2fe1c52f74d080dfcb26f3ea79b2d0231321ba
Grzegorz Kłosiński <Grzegorz Kosiski>
2018-05-03 21:35:19 +0200

    Began changing the layout

--- cbc.js
+++ cbc.js
@@ -1,27 +1,38 @@
 function cbc(plaintext){
     var iv=bigInt.randBetween(1,p.prev());
     
-    var ciphertextA = ecbEncrypt(plaintext, a, p, iv);
+    var ciphertextA = cbcEncrypt(plaintext, a, p, iv);
     $('#aEncrypted').val(ciphertextA);
 
-    var ciphertextAB = ecbEncrypt(ciphertextA, b, p, iv);
+    var ciphertextAB = ecbTransform(ciphertextA, b, p);
     $('#bEncrypted').val(ciphertextAB);
 
-    var ciphertextABA = ecbEncrypt(ciphertextAB, aInv, p, iv);
+    var ciphertextABA = cbcDecrypt(ciphertextAB, aInv, p, iv);
     $('#aDecrypted').val(ciphertextABA);
 
-    var ciphertextABAB = ecbEncrypt(ciphertextABA, bInv, p, iv);
+    var ciphertextABAB = ecbTransform(ciphertextABA, bInv, p);
     $('#outputEncoded').val(ciphertextABAB);
 
     return ciphertextABAB;
 }
 
-function ecbEncrypt(numbers, exponent, modulus, iv) {
+function cbcEncrypt(numbers, exponent, modulus, iv) {
     var result = [];
 
-    result.push(iv.xor(numbers[0].modPow(exponent, modulus)))
+    result.push(iv.xor(numbers[0]).modPow(exponent, modulus));
     for (var i = 1; i < numbers.length; i++) {
-        result.push(numbers[i-1].xor(numbers[i].modPow(exponent, modulus)));
+        result.push(numbers[i-1].xor(numbers[i]).modPow(exponent, modulus));
+    }
+
+    return result;
+}
+
+function cbcDecrypt(numbers, exponent, modulus, iv) {
+    var result = [];
+
+    result.push(numbers[0].modPow(exponent, modulus).xor(iv));
+    for (var i = 1; i < numbers.length; i++) {
+        result.push(numbers[i].modPow(exponent, modulus).xor(numbers[i-1]));
     }
 
     return result;
