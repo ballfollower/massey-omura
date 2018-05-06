373c8f5460a66aa4526a0dc7b79eea67429cf923
Gregdeg <gregdeg@op.pl>
2018-05-06 12:54:29 +0200

    Finished OFB. Buggy yet.

--- ofb.js
+++ ofb.js
@@ -1,15 +1,33 @@
-function ofb(plaintext){
-//	var ciphertextA = ecbTransform(plaintext, a, p);
-//	$('#aEncrypted').val(ciphertextA);
-//
-//	var ciphertextAB = ecbTransform(ciphertextA, b, p);
-//	$('#bEncrypted').val(ciphertextAB);
-//
-//	var ciphertextABA = ecbTransform(ciphertextAB, aInv, p);
-//	$('#aDecrypted').val(ciphertextABA);
-//
-//	var ciphertextABAB = ecbTransform(ciphertextABA, bInv, p);
-//	$('#outputEncoded').val(ciphertextABAB);
+function ofb(plaintext) {
+    var ciphertextA = ofbEncrypt(plaintext, a, p, iv);
+    $('#aEncrypted').val(ciphertextA);
 
-	return ciphertextABAB;
+    var ciphertextAB = ofbEncrypt(ciphertextA, b, p, iv);
+    $('#bEncrypted').val(ciphertextAB);
+
+    var ciphertextABA = ofbEncrypt(ciphertextAB, aInv, p, iv);
+    $('#aDecrypted').val(ciphertextABA);
+
+    var ciphertextABAB = ofbEncrypt(ciphertextABA, bInv, p, iv);
+    $('#outputEncoded').val(ciphertextABAB);
+
+    return ciphertextABAB;
+}
+
+function ofbEncrypt(numbers, exponent, modulus, iv) {
+    var result = [];
+    var output;
+
+    output = iv.modPow(exponent, modulus);
+    result.push(
+            output.xor(numbers[0])
+            );
+    for (var i = 1; i < numbers.length; i++) {
+        output = output.modPow(exponent, modulus);
+        result.push(
+                numbers[i].xor(output)
+                );
+    }
+
+    return result;
 }
