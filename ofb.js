c66424489c28cb5c3f70f63ab3a881f515ac7876
Gregdeg <gregdeg@op.pl>
2018-05-06 12:27:38 +0200

    Added OFB draft

---
+++ ofb.js
@@ -0,0 +1,15 @@
+function ofb(plaintext){
+//	var ciphertextA = ecbTransform(plaintext, a, p);
+//	$('#aEncrypted').val(ciphertextA);
+//
+//	var ciphertextAB = ecbTransform(ciphertextA, b, p);
+//	$('#bEncrypted').val(ciphertextAB);
+//
+//	var ciphertextABA = ecbTransform(ciphertextAB, aInv, p);
+//	$('#aDecrypted').val(ciphertextABA);
+//
+//	var ciphertextABAB = ecbTransform(ciphertextABA, bInv, p);
+//	$('#outputEncoded').val(ciphertextABAB);
+
+	return ciphertextABAB;
+}
