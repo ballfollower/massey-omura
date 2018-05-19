973b9c6dae169c6f369a0ab88ef3e5c398642fc2
Gregdeg <gregdeg@op.pl>
2018-05-19 20:57:38 +0200

    Moved code from ecb.js to code.js

--- code.js
+++ code.js
@@ -157,3 +157,28 @@ function decode(numbers, chars_per_block, bits_per_char) {
     return output;
 }
 
+function ecb(plaintext) {
+    var ciphertextA = ecbTransform(plaintext, a, p);
+    $('#aEncrypted').val(ciphertextA);
+
+    var ciphertextAB = ecbTransform(ciphertextA, b, p);
+    $('#bEncrypted').val(ciphertextAB);
+
+    var ciphertextABA = ecbTransform(ciphertextAB, aInv, p);
+    $('#aDecrypted').val(ciphertextABA);
+
+    var ciphertextABAB = ecbTransform(ciphertextABA, bInv, p);
+    $('#outputEncoded').val(ciphertextABAB);
+
+    return ciphertextABAB;
+}
+
+function ecbTransform(numbers, exponent, modulus) {
+    var result = [];
+
+    for (var i = 0; i < numbers.length; i++) {
+        result.push(numbers[i].modPow(exponent, modulus));
+    }
+
+    return result;
+}
